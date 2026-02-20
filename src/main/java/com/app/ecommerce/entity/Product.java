635f8736e3ade681cea4ca46088c07d942063d91
SaiSantosh13072001 <saisantoshpanigrahi56@gmail.com>
2026-02-20 15:48:28 +0530

    Add Product entity

---
+++ src/main/java/com/app/ecommerce/entity/Product.java
@@ -0,0 +1,24 @@
+package com.app.ecommerce.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+
+@Entity
+@Table(name = "products")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+    private String description;
+    private double price;
+    private int stock;
+    private String imageUrl;
+}
